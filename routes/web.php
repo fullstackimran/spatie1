7f6e3b4721888d772abe80eb31d7c5efbec244d5
imran malik <fullstackimran@gmail.com>
2024-01-03 17:32:25 +0500

    2nd commit

--- routes/web.php
+++ routes/web.php
@@ -44,4 +44,7 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
+Route::get("/imran", function () {
+    return "malik";
+});
